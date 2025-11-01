9092b7c22b6c77fc383ebb2a9461e8c5100d4710
peppe2412 <peppeterrazzano79@gmail.com>
2025-11-01 18:24:43 +0100

    Front-end 404 page

--- views/error/404.php
+++ views/error/404.php
@@ -2,14 +2,16 @@
 ob_clean();
 ?>
 
-<section class="container mt-5">
+<section class="container mt-5 mb-5">
     <div class="row justify-content-center">
         <div class="col-12 col-lg-8 text-center">
-            <h1>404</h1>
+            <h1 class="display-1">404</h1>
             <h2>Pagina non trovata!</h2>
-            <img class="img_404" src="https://i.etsystatic.com/36104253/r/il/312d48/5128189533/il_fullxfull.5128189533_t29w.jpg" alt="LOL troll">
             <div class="mt-4">
-                <a href="/">Torna alla home</a>
+                <img class="img_404" src="/public/media/troll-LOL.png" alt="LOL troll">
+            </div>
+            <div class="mt-4">
+                <a href="/" class="link-error-page fs-3">Torna alla home</a>
             </div>
         </div>
     </div>
